7723459923dcf69cc07f2ccd8f5619fa50020105
zqian15 <xhdp123@126.com>
2024-08-06 09:59:51 +0800

    imporve: player button animation

--- src/components/PlayerControls.tsx
+++ src/components/PlayerControls.tsx
@@ -1,8 +1,16 @@
 import { colors } from '@/constants/tokens'
 import { FontAwesome6 } from '@expo/vector-icons'
-import { memo } from 'react'
+import { memo, useEffect, useState } from 'react'
 import { StyleSheet, TouchableOpacity, View, ViewStyle } from 'react-native'
-import TrackPlayer, { useIsPlaying } from 'react-native-track-player'
+import Animated, {
+	Easing,
+	runOnJS,
+	useAnimatedStyle,
+	useSharedValue,
+	withSpring,
+	withTiming,
+} from 'react-native-reanimated'
+import TrackPlayer from 'react-native-track-player'
 
 type PlayerControlsProps = {
 	style?: ViewStyle
@@ -27,20 +35,85 @@ export const PlayerControls = memo(({ style }: PlayerControlsProps) => {
 	)
 })
 
-export const PlayPauseButton = memo(({ style, iconSize = 48 }: PlayerButtonProps) => {
-	const { playing } = useIsPlaying()
+export const PlayPauseButton = memo(
+	({ style, iconSize = 48, onPress }: PlayerButtonProps & { onPress?: () => void }) => {
+		const [localPlaying, setLocalPlaying] = useState(true)
+		const animation = useSharedValue(1)
+		const shadowAnimation = useSharedValue(0)
 
-	return (
-		<View style={[{ height: iconSize }, style]}>
-			<TouchableOpacity
-				activeOpacity={0.85}
-				onPress={playing ? TrackPlayer.pause : TrackPlayer.play}
-			>
-				<FontAwesome6 name={playing ? 'pause' : 'play'} size={iconSize} color={colors.text} />
-			</TouchableOpacity>
-		</View>
-	)
-})
+		useEffect(() => {
+			if (localPlaying) {
+				TrackPlayer.play()
+			} else {
+				TrackPlayer.pause()
+			}
+		}, [localPlaying])
+
+		const togglePlayingState = () => {
+			setLocalPlaying((prev) => !prev)
+		}
+
+		const handlePress = () => {
+			shadowAnimation.value = withTiming(1, {
+				duration: 40,
+				easing: Easing.inOut(Easing.ease),
+			})
+
+			animation.value = withTiming(
+				0,
+				{
+					duration: 40,
+					easing: Easing.inOut(Easing.ease),
+				},
+				(finished) => {
+					if (finished) {
+						runOnJS(togglePlayingState)()
+						animation.value = withSpring(1.1, {
+							damping: 15,
+							stiffness: 150,
+							mass: 1,
+						})
+						shadowAnimation.value = withTiming(0, {
+							duration: 40,
+							easing: Easing.inOut(Easing.ease),
+						})
+					}
+				},
+			)
+			if (onPress) {
+				onPress()
+			}
+		}
+
+		const animatedStyle = useAnimatedStyle(() => {
+			return {
+				transform: [{ scale: animation.value }],
+			}
+		})
+
+		const shadowStyle = useAnimatedStyle(() => {
+			return {
+				opacity: shadowAnimation.value ? 0.8 : 0,
+				transform: [{ scale: shadowAnimation.value ? 1 : 0 }],
+			}
+		})
+
+		return (
+			<View style={[{ height: iconSize }, style]}>
+				<TouchableOpacity activeOpacity={0.85} onPress={handlePress} style={styles.buttonContainer}>
+					<Animated.View style={[styles.shadow, shadowStyle]} />
+					<Animated.View style={animatedStyle}>
+						<FontAwesome6
+							name={localPlaying ? 'pause' : 'play'}
+							size={iconSize}
+							color={colors.text}
+						/>
+					</Animated.View>
+				</TouchableOpacity>
+			</View>
+		)
+	},
+)
 
 export const SkipToNextButton = memo(({ iconSize = 30 }: PlayerButtonProps) => {
 	return (
@@ -53,7 +126,7 @@ export const SkipToNextButton = memo(({ iconSize = 30 }: PlayerButtonProps) => {
 export const SkipToPreviousButton = ({ iconSize = 30 }: PlayerButtonProps) => {
 	return (
 		<TouchableOpacity activeOpacity={0.7} onPress={() => TrackPlayer.skipToPrevious()}>
-			<FontAwesome6 name={'backward'} size={iconSize} color={colors.text} />
+			<FontAwesome6 name="backward" size={iconSize} color={colors.text} />
 		</TouchableOpacity>
 	)
 }
@@ -65,6 +138,17 @@ const styles = StyleSheet.create({
 	row: {
 		flexDirection: 'row',
 		justifyContent: 'space-evenly',
+		alignItems: 'center',
+	},
+	shadow: {
+		position: 'absolute',
+		width: 60,
+		height: 60,
+		borderRadius: 30,
+		backgroundColor: 'rgba(0, 0, 0, 0.1)',
+	},
+	buttonContainer: {
+		justifyContent: 'center',
 		alignItems: 'center',
 	},
 })
