5191811919d02746ffb8af73becab909bb4101bf
Zhen Qian <zqian15@asu.edu>
2025-02-13 18:41:16 +0800

    fix: remove console.log

--- src/app/player.tsx
+++ src/app/player.tsx
@@ -208,37 +208,6 @@ const PlayerScreen = () => {
 		if (!activeTrackObj?.formatedTitle) return
 
 		loadLrc(activeTrackObj.filename, activeTrackObj.formatedTitle)
-
-		const parseTime = (timeString: { split: (arg0: string) => [any, any] }) => {
-			const [minutes, seconds] = timeString.split(':')
-			const [secs, millis] = seconds.split('.')
-
-			return parseInt(minutes) * 60 + parseInt(secs) + parseInt(millis || 0) / 1000
-		}
-
-		// searchSongsViaNetease(params).then((el) => {
-		// 	const id = el?.result?.songs?.[0]?.id
-
-		// 	searchLyricViaNetease({ id }).then((lyric) => {
-		// 		const raw = lyric?.lrc?.lyric
-		// 		const formatedLyric = raw
-		// 			.split('\n')
-		// 			.map((l: string) => {
-		// 				const regex = /\[([0-9]{2,3}:[0-9]{2,3}\.[0-9]{2,3})\]\s*(.*)/
-		// 				const match = l.match(regex)
-		// 				if (match) {
-		// 					const timestamp = parseTime(match[1])
-		// 					const content = match[2]
-		// 					return {
-		// 						time: timestamp,
-		// 						line: content,
-		// 					}
-		// 				}
-		// 			})
-		// 			.filter((el: any) => el)
-		// 		setLyrics(formatedLyric)
-		// 	})
-		// })
 	}, [activeTrackObj])
 
 	const renderScene = ({ route }) => {
--- src/helpers/LyricLoader/index.ts
+++ src/helpers/LyricLoader/index.ts
@@ -2,7 +2,7 @@
  * @Author: Zhen Qian zqian15@asu.edu
  * @Date: 2025-02-12 14:59:07
  * @LastEditors: Zhen Qian zqian15@asu.edu
- * @LastEditTime: 2025-02-13 16:53:30
+ * @LastEditTime: 2025-02-13 18:40:21
  * @FilePath: /TuneSync/src/helpers/LyricLoader/index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -17,17 +17,9 @@ const parseTime = (timeString: { split: (arg0: string) => [any, any] }) => {
 	return parseInt(minutes) * 60 + parseInt(secs) + parseInt(millis || 0) / 1000
 }
 async function getOnlineLyric(params) {
-	console.log('getOnlineLyric', params)
-
 	const el = await searchSongsViaNetease(params)
-	console.log('el', el)
-
 	const id = el?.result?.songs?.[0]?.id
-	console.log('id', id)
-
 	const lyric = await searchLyricViaNetease({ id })
-	console.log('lyric', lyric)
-
 	const raw = lyric?.lrc?.lyric
 	const formatedLyric = raw
 		.split('\n')
@@ -59,8 +51,6 @@ export function useLrcLoader(activeTrackObj: any = { source: {} }) {
 
 		try {
 			const checkExist = await webdavClient.exists(lrcPath)
-			console.log('checkExist', checkExist)
-
 			if (checkExist) {
 				const res = await webdavClient.getFileContents(lrcPath, { format: 'text' })
 				const lrcText = res.toString()
@@ -69,8 +59,6 @@ export function useLrcLoader(activeTrackObj: any = { source: {} }) {
 				setLyrics(parsed)
 			} else {
 				const parsed = await getOnlineLyric({ s: name })
-				console.log('parsed2222', parsed)
-
 				setLyrics(parsed)
 			}
 		} catch (err) {
