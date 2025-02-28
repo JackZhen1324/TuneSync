9b1c0f2a2d1cc7a2a77cff1d006cdcbc4e368930
zhen qian <xhdp123@126.com>
2025-02-28 15:52:53 +0800

    feat: support local lrc file parsing

--- src/helpers/LyricLoader/index.ts
+++ src/helpers/LyricLoader/index.ts
@@ -2,10 +2,11 @@
  * @Author: Zhen Qian zqian15@asu.edu
  * @Date: 2025-02-12 14:59:07
  * @LastEditors: Zhen Qian zqian15@asu.edu
- * @LastEditTime: 2025-02-13 18:40:21
+ * @LastEditTime: 2025-02-19 14:39:22
  * @FilePath: /TuneSync/src/helpers/LyricLoader/index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
+// eslint-disable
 import useWebdavClient from '@/hooks/useWebdavClient'
 import { searchLyricViaNetease, searchSongsViaNetease } from '@/service/neteaseData'
 import { useState } from 'react'
@@ -16,16 +17,17 @@ const parseTime = (timeString: { split: (arg0: string) => [any, any] }) => {
 
 	return parseInt(minutes) * 60 + parseInt(secs) + parseInt(millis || 0) / 1000
 }
-async function getOnlineLyric(params) {
-	const el = await searchSongsViaNetease(params)
+async function getOnlineLyric(params: { s: any }) {
+	const el = (await searchSongsViaNetease(params)) as any
 	const id = el?.result?.songs?.[0]?.id
-	const lyric = await searchLyricViaNetease({ id })
+	const lyric = (await searchLyricViaNetease({ id })) as any
+
 	const raw = lyric?.lrc?.lyric
 	const formatedLyric = raw
 		.split('\n')
 		.map((l: string) => {
 			const regex = /\[([0-9]{2,3}:[0-9]{2,3}\.[0-9]{2,3})\]\s*(.*)/
-			const match = l.match(regex)
+			const match = l.match(regex) as any
 			if (match) {
 				const timestamp = parseTime(match[1])
 				const content = match[2]
@@ -42,10 +44,10 @@ export function useLrcLoader(activeTrackObj: any = { source: {} }) {
 	const { source } = activeTrackObj
 	const [lyrics, setLyrics] = useState<{ time: number; line: string }[]>([])
 	const webdavClient = useWebdavClient(source)
-	const loadLrc = async (filepath, name) => {
+	const loadLrc = async (filepath: string, name: any) => {
 		const audioExtensions = ['mp3', 'wav', 'flac', 'aac', 'ogg', 'm4a', 'wma'] // 常见音频扩展名
 		const regex = new RegExp(`\\.(?:${audioExtensions.join('|')})$`)
-		const match = filepath.match(regex)
+		const match = filepath.match(regex) as any
 		const basePath = filepath.replace(match, '')
 		const lrcPath = `${basePath}.lrc`
 
